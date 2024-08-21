4dd1d71d5362f11d4008ce7cf82005e1b29e01f8
vishalsavarna <vishalsavarna@gmail.com>
2024-08-21 18:47:12 +0530

    Implement Swiper Autoplay for smooth Transition

--- frontend/src/pages/Home/Hero/HeroContainer.jsx
+++ frontend/src/pages/Home/Hero/HeroContainer.jsx
@@ -1,16 +1,14 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/effect-creative";
-import { EffectCreative } from "swiper";
-// import './Temp.css'
+import { EffectCreative, Autoplay } from "swiper";
 import Hero from './Hero';
 import Hero2 from './Hero2';
-const HeroContainer = () => {
 
+const HeroContainer = () => {
     return (
         <section className=''>
-            {/* <Hero /> */}
             <Swiper
                 grabCursor={true}
                 effect={"creative"}
@@ -24,13 +22,16 @@ const HeroContainer = () => {
                         translate: ["120%", 0, -500],
                     },
                 }}
-                modules={[EffectCreative]}
+                modules={[EffectCreative, Autoplay]}
                 className="mySwiper5"
                 loop={true}
                 autoplay={{
-                    delay: 250,
-                    disableOnInteraction: false
+                    delay: 3000, // Time in milliseconds between slides (3 seconds)
+                    disableOnInteraction: false, // Allow autoplay to continue even if user interacts with the Swiper
                 }}
+                speed={800} // Transition speed in milliseconds (adjust as needed)
+                // Add easing function if needed
+                // To use a custom easing function, you may need to apply custom CSS or use Swiper's transition configurations
             >
                 <SwiperSlide>
                     <Hero />
@@ -39,7 +40,6 @@ const HeroContainer = () => {
                     <Hero2 />
                 </SwiperSlide>
             </Swiper>
-
         </section>
     );
 };
